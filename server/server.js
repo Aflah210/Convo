0914aa015866d41cf56ada476418359efebe10c3
aflah haami <aflahhaami@aflahs-MacBook-Air.local>
2025-12-21 21:12:41 +0530

    feat(auth): add user register & login with JWT

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,8 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
 
 // Health check route
 app.get("/api/health", (req, res) => {
